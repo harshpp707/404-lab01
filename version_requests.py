fd211fc20621110f6c44373822263d15d4f8a792
harshpp707 <73919744+harshpp707@users.noreply.github.com>
2023-01-09 22:05:07 -0700

    done

--- version_requests.py
+++ version_requests.py
@@ -1,4 +1,5 @@
 import requests
 
 # print(requests.__version__)
-print(requests.get("https://www.google.com/").text)
+# print(requests.get("https://www.google.com/").text)
+print(requests.get("https://raw.githubusercontent.com/harshpp707/404-lab01/main/version_requests.py").text)
