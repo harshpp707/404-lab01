6801375c525299611bcc59e8f35a2d6b78ce70b3
harshpp707 <73919744+harshpp707@users.noreply.github.com>
2023-01-09 22:09:23 -0700

    own_source_code done

--- own_source_code.py
+++ own_source_code.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get("https://raw.githubusercontent.com/harshpp707/404-lab01/main/version_requests.py").text)
+print(requests.get("https://raw.githubusercontent.com/harshpp707/404-lab01/main/own_source_code.py").text)
